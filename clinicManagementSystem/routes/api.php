f2540a5d5f026a53f2eedb6a23f06bbfffb6308d
shakya <shakya980528@gmail.com>
2023-06-11 07:49:29 -0700

    Adding the routes

--- clinicManagementSystem/routes/api.php
+++ clinicManagementSystem/routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('addPatient', [\App\Http\Controllers\Controller::class, 'addPatient'])->name('api.addPatient');
+Route::post('addRecord/{id}', [\App\Http\Controllers\Controller::class, 'addRecord'])->name('api.addRecord');
+Route::post('getTotalBillAmount', [\App\Http\Controllers\Controller::class, 'getTotalBillAmount'])->name('api.getTotalBillAmount');
+Route::get('getPatients', [\App\Http\Controllers\Controller::class, 'getPatients'])->name('api.getPatients');
