fbb1cf973911308d2f28ccaa4dc828e5c1c6e0fc
shakya <shakya980528@gmail.com>
2023-06-11 00:36:24 -0700

    Creating add patient function

--- clinicManagementSystem/app/Http/Controllers/Controller.php
+++ clinicManagementSystem/app/Http/Controllers/Controller.php
@@ -5,8 +5,37 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use App\Models\Patient;
+
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    // add patient function
+
+    public function addPatient(Request $request)
+    {
+        // Validate the required fields
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'birthday' => 'required',
+            'contact_no' => 'required',
+            'photo' => 'required',
+            'nic' => 'required',
+            'notes' => 'required',
+        ]);
+
+        $patient_data = Patient::create([
+            'name' => $request->input('name'),
+            'birthday' => $request->input('birthday'),
+            'contact_no' => $request->input('contact_no'),
+            'photo' => $request->input('photo'),
+            'nic' => $request->input('nic'),
+            'notes' => $request->input('notes'),
+        ]);
+
+        return response()->json($patient_data, 201);
+    }
 }
